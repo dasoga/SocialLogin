85caee4c5cf3c3a31165e83c1fb00cdf47dab853
Dante Solorio <contact@deipi.com>
2016-10-29 00:24:39 -0500

    Custom login button

--- SocialLogin/ViewController.swift
+++ SocialLogin/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import FBSDKLoginKit
 
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,6 +19,18 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         view.addSubview(fbLoginButton)
         fbLoginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
         fbLoginButton.delegate = self
+        fbLoginButton.readPermissions = ["email", "public_profile"]
+        
+        // Add custom fb login button
+        let customFbLoginButton = UIButton(type: .system)
+        customFbLoginButton.backgroundColor = .blue
+        customFbLoginButton.setTitle("FB Login", for: .normal)
+        customFbLoginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        customFbLoginButton.setTitleColor(.white, for: .normal)
+        customFbLoginButton.frame = CGRect(x: 16, y: 116, width: view.frame.width - 32, height: 50)
+        customFbLoginButton.addTarget(self, action: #selector(handleCustomLoginAction), for: .touchUpInside)
+        view.addSubview(customFbLoginButton)
+        
     }
     
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
@@ -28,11 +40,39 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         }
         
         print("Successfully logged in with facebook...")
+        showEmailAddress()
     }
     
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
         print("Did log out of facebook")
     }
+    
+    fileprivate func showEmailAddress(){
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields":"id, name, email"]).start { (connection, result, err) in
+            if err != nil{
+                print("Failed to start graph request ",err)
+                return
+            }
+            
+            print(result)
+        }
+    }
+    
+    // MARK: - Handle functions
+    func handleCustomLoginAction(){
+        FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self)
+        { (result, err) in
+            if err != nil{
+                print("Failed custom fb login ",err)
+                return
+            }
+            
+            print(result?.token.tokenString)
+            self.showEmailAddress()
+        }
+    }
+    
+    
 }
 
